dc52333c283907531ee06e053b0ae0f91363ad2f
Minsun <58019747+minisundev@users.noreply.github.com>
2024-02-07 15:26:01 +0900

    [level 2] Title: 프로세스, Time: 0.78 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,32 +1,43 @@
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Arrays;
-import java.util.Collections;
 
 class Solution {
-    public int solution(int[] p, int location) {
+    public int solution(int[] priorities, int location) {
         //등록, 뭐가 몇번째에 실행되었는지 알려면 Map 써야할듯
         Map<Integer,Integer> map = new HashMap<Integer,Integer>();
         Map<Integer,Integer> order = new HashMap<Integer,Integer>();
-
-        Integer [] priorities = new Integer[p.length];
-        for(int i = 0; i<p.length; i++){
-            map.put(i,p[i]);
-            priorities[i] = p[i];
+        for(int i = 0; i<priorities.length; i++){
+            map.put(i,priorities[i]);
         }
+
+        int max = 0;
+        //find max
+        ArrayList<Map.Entry<Integer,Integer>> list = new ArrayList<>(map.entrySet()); 
+        for(Map.Entry<Integer,Integer> entry : list){
+                            
+                Integer key = entry.getKey();
+                Integer index = entry.getValue();
+            
+            if(index>max){
+                max = index;
+            }
+        }
+        System.out.println("max: "+max);
         
-        Arrays.sort(priorities, Collections.reverseOrder());
-        
+        //인덱스
         int idx = 0;
         int count = 1;
+        //find process
         //order에 포함됐으면 건너뜀
-        while(count<=priorities.length){
-            if(order.getOrDefault(idx,-1)==-1){
-                if(map.get(idx)>=priorities[count-1]){
+        while(idx>=priorities.length){
+            if(order.getOrDefault(idx,-1)!=-1){
+                if(map.get(idx)>=max){
+                    System.out.println("idx: "+idx+" count: "+count);
                     order.put(idx,count);
+                    map.remove(idx);
                     count++;
                 }
-
             }
             idx = (idx+1)%priorities.length;
         }
