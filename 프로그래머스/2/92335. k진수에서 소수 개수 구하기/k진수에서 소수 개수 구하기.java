e0198377101043d1bccbdde1f5e628be9ea7ac79
Minsun <58019747+minisundev@users.noreply.github.com>
2024-02-19 10:13:54 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 10.11 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.lang.Math;
+
+class Solution {
+    public int solution(int n, int k) {
+        
+        int answer = 0;
+        ArrayList<Long> splits = new ArrayList<>();
+        
+        //k진수로 변환
+        String digitK = toK(n, k);
+
+        // 0으로 쪼개기
+        String [] split = digitK.split("0");
+        
+        //소수인지 확인
+        for(int i = 0; i < split.length; i++){
+            String s = split[i];
+            if(s.equals("")){
+                continue;
+            }
+            long candidate = Long.parseLong(s);
+            if(isPrime(candidate)){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+    
+    //10진수 -> k진수
+    public String toK(int n, int k) {
+        String digitK = "";
+        for (int i = n; i > 0; i = i / k) {
+            digitK = i % k + digitK;
+        }
+        return digitK;
+    }
+
+    //k진수 -> 10진수
+    public long fromK(String s, int k){
+        long digit = 0;
+        for(long i = 0;i<s.length();i++){
+            long place = 1;
+            for(long j=0; j<i;j++){
+                place *= k;
+            }
+            digit += place*Long.parseLong(""+s.charAt(s.length()-1-(int)i));
+        }
+        return digit;
+    }
+    
+    //소수인지 확인
+    public boolean isPrime(long number){
+        if(number<=1){
+            return false;
+        }
+        
+        for (long i = 2; i <= Math.sqrt(number); i++) {
+             // double target = number;
+             // double divider = i;
+             // double quote = target / divider;
+             // long intQuote = (long) quote;
+             // if (quote - intQuote == 0) {
+             //        continue;
+             // }
+            if(number%i==0){
+                return false;
+            }
+            
+        }
+        
+        return true;
+
+    }
+}
