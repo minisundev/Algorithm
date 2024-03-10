b2014c44f97e9cf455cfda475c59a12f5540cc7e
Minsun <58019747+minisundev@users.noreply.github.com>
2024-03-10 23:31:52 +0900

    [Silver II] Title: 유기농 배추, Time: 392 ms, Memory: 29896 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,111 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int [][] arr;
+    static boolean [][] checked;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int cases = Integer.parseInt(scanner.nextLine());
+        for(int idx = 0; idx<cases; idx++){
+            String s = scanner.nextLine();
+            String [] t = s.split(" ");
+            int n = Integer.parseInt(t[0]);
+            int m = Integer.parseInt(t[1]);
+            int k = Integer.parseInt(t[2]);
+
+            arr = new int [n][m];
+            checked = new boolean[n][m];
+            int [][] starters = new int [k][2];
+            int count = 0;
+
+            for(int i=0;i<k;i++){
+                String str = scanner.nextLine();
+                String [] temp = str.split(" ");
+                starters[i][0] = Integer.parseInt(temp[0]);
+                starters[i][1] = Integer.parseInt(temp[1]);
+                arr[starters[i][0]][starters[i][1]] = 1;
+            }
+
+            for(int l=0;l<starters.length;l++){
+                int i = starters[l][0];
+                int j = starters[l][1];
+                if(arr[i][j]==1&&!checked[i][j]) {
+                    search(i, j);
+                    count++;
+                }
+            }
+            list.add(count);
+        }
+
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+    }
+
+    public static void search(int n, int m){
+
+        Queue<Integer> xq = new LinkedList<>();
+        Queue<Integer> yq = new LinkedList<>();
+
+        xq.add(n);
+        yq.add(m);
+        checked[n][m] = true;
+
+        while(!xq.isEmpty()&&!yq.isEmpty()) {
+
+            int x = xq.poll();
+            int y = yq.poll();
+
+                int nextX = x+1;
+                int nextY = y;
+
+                if(nextX<arr.length){
+                    if(arr[nextX][nextY]==1&&!checked[nextX][nextY]){
+                        xq.add(nextX);
+                        yq.add(nextY);
+                        checked[nextX][nextY] = true;
+                    }
+                }
+
+            nextX = x-1;
+            nextY = y;
+
+            if(nextX>=0){
+                if(arr[nextX][nextY]==1&&!checked[nextX][nextY]){
+                    xq.add(nextX);
+                    yq.add(nextY);
+                    checked[nextX][nextY] = true;
+                }
+            }
+
+            nextX = x;
+            nextY = y+1;
+
+            if(nextY<arr[0].length){
+                if(arr[nextX][nextY]==1&&!checked[nextX][nextY]){
+                    xq.add(nextX);
+                    yq.add(nextY);
+                    checked[nextX][nextY] = true;
+                }
+            }
+
+            nextX = x;
+            nextY = y-1;
+
+            if(nextY>=0){
+                if(arr[nextX][nextY]==1&&!checked[nextX][nextY]){
+                    xq.add(nextX);
+                    yq.add(nextY);
+                    checked[nextX][nextY] = true;
+                }
+            }
+
+        }
+    }
+}
