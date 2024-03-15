858849cd369ef764af2645aa2294663b652d131a
Minsun <58019747+minisundev@users.noreply.github.com>
2024-03-15 10:12:22 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.88 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,26 +2,12 @@ import java.util.*;
 
 class Solution {
     
-    static int [][] arr;
     static boolean [] visited;
+    static int [][] wire;
     
     public int solution(int n, int[][] wires) {
         //wires하나 잡고 그거 없을때로 양쪽 노드로 들어가서 연결된 노드 개수 찾아옴, 해당 간선의 index는 무시하고
-        arr = new int[n][n];
-        
-        for(int i=0;i<wires.length;i++){
-            int from = wires[i][0];
-            int to = wires[i][1];
-            arr[from-1][to-1] = 1;
-            arr[to-1][from-1] = 1;
-        } 
-        
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                System.out.print(arr[i][j]);
-            }
-            System.out.println();
-        }
+        wire = wires;
         
         int diff = n+1;
         visited = new boolean [n];
@@ -31,14 +17,11 @@ class Solution {
             int from = wires[i][0]-1;
             int to = wires[i][1]-1;
             
-            arr[from][to] = 0;
-            arr[to][from] = 0;
-            
             Arrays.fill(visited,false);
             
-            int temp1 = search(from, 0);
+            int temp1 = search(i,from, 0);
             
-            int temp2 = search(to, 0);
+            int temp2 = search(i,to, 0);
             int temp = temp1-temp2;
             if(temp<0){
                 temp *= -1;
@@ -47,9 +30,6 @@ class Solution {
             if(temp<diff){
                 diff = temp;
             }
-            
-            arr[from][to] = 1;
-            arr[to][from] = 1;
         }
         
         for(boolean b : visited){
@@ -62,13 +42,22 @@ class Solution {
         return diff;
     }
     
-    public int search(int node, int count){
+    public int search(int ignore, int node, int count){
         visited[node] = true;
         count++;
         
-        for(int i=0;i<arr[0].length;i++){
-            if(arr[node][i]==1&&!visited[i]){
-                count = search(i,count);
+        for(int i=0;i<wire.length;i++){
+            if(i==ignore){
+                continue;
+            }
+            int from = wire[i][0]-1;
+            int to = wire[i][1]-1;
+            
+            if(from == node&&!visited[to]){
+                count = search(i,to,count);
+            }
+            else if(to == node &&!visited[from]){
+                count = search(i,from,count);
             }
         }
         
