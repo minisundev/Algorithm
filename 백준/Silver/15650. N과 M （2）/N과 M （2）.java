f099a73d45781063baf8e4c039a01eb30d0e5c07
Minsun <58019747+minisundev@users.noreply.github.com>
2024-03-19 17:08:50 +0900

    [Silver III] Title: N과 M (2), Time: 152 ms, Memory: 15864 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static ArrayList<ArrayList<Integer>> list;
+    static int m;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList<>();
+
+        ArrayList<Integer> array = new ArrayList<>();
+
+        append(array);
+
+        for (int i=0;i<list.size();i++) {
+            ArrayList<Integer> temp = list.get(i);
+            for(int j : temp){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    static void append(ArrayList<Integer> arr){
+        if(arr.size()>=m){
+            list.add(arr);
+            return;
+        }
+        boolean [] exist = new boolean[n+1];
+
+        for(int i: arr){
+            exist[i] = true;
+        }
+
+        int last;
+        if(arr.size()==0){
+            last = 0;
+        }else{
+            last = arr.get(arr.size()-1);
+        }
+
+        for(int i=1;i<=n;i++){
+            if(!exist[i]&&last<i){
+                append(plus(arr,i));
+            }
+        }
+    }
+
+    static ArrayList<Integer> plus(ArrayList<Integer> arr, int x){
+        ArrayList<Integer> array = new ArrayList<>();
+        for(int i : arr){
+            array.add(i);
+        }
+        array.add(x);
+        return array;
+    }
+
+}
