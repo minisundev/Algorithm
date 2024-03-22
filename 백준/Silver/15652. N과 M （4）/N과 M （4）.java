f7931166f0e1ca9e4b74ff47958ab635ad1b5c60
Minsun <58019747+minisundev@users.noreply.github.com>
2024-03-22 23:17:19 +0900

    [Silver III] Title: N과 M (4), Time: 164 ms, Memory: 17812 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,64 +1,52 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static ArrayList<ArrayList<Integer>> list;
-    static int m;
+    static int[] arr;
     static int n;
+    static int m;
+
+    static StringBuilder s = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
 
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        list = new ArrayList<>();
-
-        ArrayList<Integer> array = new ArrayList<>();
-
-        append(array);
-
-        for (int i=0;i<list.size();i++) {
-            ArrayList<Integer> temp = list.get(i);
-            for(int j : temp){
-                System.out.print(j+" ");
-            }
-            System.out.println();
-        }
-
+        arr = new int[m];
+        dfs(0);
+        System.out.println(s.toString());
     }
 
-    static void append(ArrayList<Integer> arr){
-        if(arr.size()>=m){
-            list.add(arr);
+    public static void dfs(int depth) {
+
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                s.append(arr[i]);
+                s.append(' ');
+            }
+            s.append('\n');
             return;
         }
 
         int last;
-        if(arr.size()==0){
+        if(depth==0){
             last = 0;
         }else{
-            last = arr.get(arr.size()-1);
+            last = arr[depth-1];
         }
 
-        for(int i=1;i<=n;i++){
+        for (int i = 1; i <= n; i++) {
             if(last<=i){
-                append(plus(arr,i));
+                arr[depth] = i;
+                dfs(depth + 1);
             }
         }
     }
-
-    static ArrayList<Integer> plus(ArrayList<Integer> arr, int x){
-        ArrayList<Integer> array = new ArrayList<>();
-        for(int i : arr){
-            array.add(i);
-        }
-        array.add(x);
-        return array;
-    }
-
 }
