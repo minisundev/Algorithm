54cb5205ca89d30abef2502559d0aa8f72d1ab58
Minsun <58019747+minisundev@users.noreply.github.com>
2024-03-27 17:32:32 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+        int [] index = new int [data.length];
+        //data에 뭐가 들어있을지 알고 다 바꿈... 그냥 index를 저장하는 배열 만들어서 순서 저장하자?
+        //초기화
+        for(int i=0;i<data.length;i++){
+            index[i] = i;
+        }
+        //읽으면서 순서 변동, 버블 정렬
+        for(int i=data.length-1;i>0;i--){
+            for(int j = 0; j<i ;j++){
+                
+                int former = data[index[j]][col-1];
+                int latter = data[index[j+1]][col-1];
+                
+                if(former>latter){//col번째 컬럼의 값을 기준으로 오름차순 정렬
+                    swap(index,j+1,j);
+                    
+                }else if(latter==former){
+                    
+                    int before = data[index[j]][0];
+                    int after = data[index[j+1]][0];
+                    
+                    if(before<after){//첫 번째 컬럼의 값을 기준으로 내림차순 정렬
+                        swap(index,j+1,j);
+                    }
+                }
+            }
+        }
+        
+        //S_i 구하기
+        int S = -1;
+        int hash = 0;
+        for(int i = row_begin; i<= row_end; i++){
+            int idx = index[i-1];
+            int S_i = 0;
+            for(int j = 0; j< data[0].length; j++){
+                S_i += data[idx][j]%i;
+            }
+            if(S!=-1){
+                S = S_i;
+            }else{
+                hash = hash^S_i;
+            }
+            
+        }
+        
+        return hash;
+    }
+    
+    public void swap(int [] array , int a, int b){
+        int temp = array[b];
+        array[b] = array[a];
+        array[a] = temp;
+    }
+}
+
