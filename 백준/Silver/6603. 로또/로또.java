b59f17a37e17536cce82e538e6366f3859771e51
Minsun <58019747+minisundev@users.noreply.github.com>
2024-04-05 01:39:43 +0900

    [Silver II] Title: 로또, Time: 308 ms, Memory: 19196 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+
+    static int K=0;
+    static int[] arr;
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            K=sc.nextInt();	//개수
+
+            if(K==0)
+                break;
+            arr= new int[K];
+
+            for(int i=0;i<K;i++)
+                arr[i]=sc.nextInt();
+
+            search("",-1,0);
+            System.out.println();
+        }
+
+    }// main()
+
+    public static void search(String s ,int max, int count){
+        if(6==count){
+            System.out.println(s);
+            return;
+        }
+
+        for(int i=max+1;i<arr.length;i++){
+            search(s+arr[i]+" ",i,count+1);
+        }
+    }
+
+}
