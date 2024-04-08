17fd264adcdd106e685506bd22c105cf432e8dff
Minsun <58019747+minisundev@users.noreply.github.com>
2024-04-08 10:09:43 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.03 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/README.md
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/README.md
@@ -1,10 +1,10 @@
 # [level 2] 혼자 놀기의 달인 - 131130 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131130#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131130) 
 
 ### 성능 요약
 
-메모리: 71.2 MB, 시간: 0.24 ms
+메모리: 77.9 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 01일 13:19:23
+2024년 04월 08일 10:09:40
 
 ### 문제 설명
 
--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -16,13 +16,7 @@ class Solution {
         int secondmax = 0;
         
         for(int i=0;i<visit.length;i++){
-            //이전 탐색 저장 : 오픈된 박스 치우기 
-            for(int j=0;j<visit.length;j++){
-                if(secondvisit[j]){
-                    visit[j] = true;
-                }
-                secondvisit[j] = false;
-            }
+          
             result = 0;
             if(!visit[i]){
                 search(i,0);
@@ -44,9 +38,9 @@ class Solution {
             return;
         }
         
-        secondvisit[i] = true;
+        visit[i] = true;
         
-        if(!secondvisit[boxes[i]-1]){
+        if(!visit[boxes[i]-1]){
             search(boxes[i]-1,count+1);
         }else{
             result = count+1;
