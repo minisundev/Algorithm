ad581349be02451394584525c92008d4b5a14ecc
Minsun <58019747+minisundev@users.noreply.github.com>
2024-04-29 13:57:10 +0900

    [level 2] Title: 영어 끝말잇기, Time: 13.70 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        
+        Set<String> set = new HashSet<>();
+        
+        int last = 0;
+        String lastString = words[0];
+        set.add(lastString);
+        
+        for(int i=1;i<words.length;i++){
+            last = i;
+            String s = words[i];
+            if(s.charAt(0)!=lastString.charAt(lastString.length()-1)){
+                break;
+            }
+            if(set.contains(s)){
+                break;
+            }
+            set.add(s);
+            lastString = s;
+        }
+        
+        System.out.println(last+" "+lastString);
+        int turn;
+        int person;
+        
+        if(last==words.length-1&&lastString.equals(words[words.length-1])){
+   
+            turn = 0;
+            person = 0;
+            
+        }else{
+            person = last%n+1;
+            turn = last/n+1;
+        }
+        
+        int [] answer = {person,turn};
+        return answer;
+    }
+}
