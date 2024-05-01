548524d752a30adb97587567833b71620eabb1bd
Minsun <58019747+minisundev@users.noreply.github.com>
2024-05-01 12:03:48 +0900

    [Silver II] Title: 격자상의 경로, Time: 124 ms, Memory: 14172 KB, Score: 9 point -BaekjoonHub

---
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken()); //반드시 지나가야 하는 칸
+
+        int mustX;
+        int mustY;
+
+        if(k!=0){
+            mustX = k/m;
+            mustY = k%m;
+            if(mustY>0){
+                mustX++;//나머지가 있다면 다음 행이 됨
+            }
+        }else{
+            mustX = 1;
+            mustY = 1;
+        }
+
+        int result = combination(mustX-1, mustY-1);
+        result *= combination(n-mustX, m-mustY);
+
+        System.out.println(result);
+    }
+
+    public static int combination(int n_r, int r){
+
+        int max = Math.max(r,n_r);
+        int min = Math.min(r,n_r);
+        int combination = 1;
+        for(int i=n_r+r; i>max; i--){
+            combination *= i;
+        }
+        for(int i=min; i>=2; i--){
+            combination /= i;
+        }
+
+        return combination;
+    }
+}
