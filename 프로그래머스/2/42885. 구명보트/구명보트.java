be4a64becf229fa9d46cf006e8b8362aeb232a66
Minsun <58019747+minisundev@users.noreply.github.com>
2024-05-02 12:00:05 +0900

    [level 2] Title: 구명보트, Time: 21.24 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int count = 0;
+
+        int left = 0;  // 제일 가벼운 사람부터
+        int right = people.length - 1;  // 제일 무거운 사람부터
+
+        while (left <= right) {
+            int sum = 0;
+            sum += people[right--];//무거운 사람 일단 태움
+            
+            if(sum + people[left]<=limit&&left<=right){//가벼운 사람 태울 수 있으면 태움
+                sum += people[left];
+                left++;
+            }
+            count++;//보트 증가시킴
+        }
+
+        return count;
+    }
+}
