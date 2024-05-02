d161f0c44f208db9fb3b6cfaa81b5cf201800e2b
Minsun <58019747+minisundev@users.noreply.github.com>
2024-05-02 23:57:23 +0900

    [Silver II] Title: 이항 계수 2, Time: 140 ms, Memory: 18208 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
+
+public class Main {
+    public static final int div = 10007;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] Triangle = new int[N+1][N+1];
+
+        for(int i = 0; i < Triangle.length; i++) {
+            for(int j = 0; j <= i; j++) {
+                if(i == j || j == 0) Triangle[i][j] = 1;
+                else Triangle[i][j] = (Triangle[i-1][j-1] + Triangle[i-1][j]) % 10007;
+            }
+        }
+        System.out.println(Triangle[N][K]);
+    }
+}
