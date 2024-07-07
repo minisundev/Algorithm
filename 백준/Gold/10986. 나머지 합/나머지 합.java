88b42a9107fe840f768f89cbd6d51386f9a6f342
Minsun <58019747+minisundev@users.noreply.github.com>
2024-07-07 15:52:35 +0900

    [Gold III] Title: 나머지 합, Time: 440 ms, Memory: 112948 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -2,39 +2,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        long[] arr = new long[N+1];
+        int mod = 0;
+        long [] modulo = new long[M];
         long count = 0;
-
-        HashMap<Long, Long> remainderMap = new HashMap<>();
-        remainderMap.put(0L, 1L);
-
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            int now = Integer.parseInt(st.nextToken());
-            arr[i] = arr[i-1] + now;
-            long remainder = arr[i] % M;
 
-            if (remainder < 0) {
-                remainder += M;
-            }
-
-            if (remainderMap.containsKey(remainder)) {
-                count += remainderMap.get(remainder);
-            }
-
-            remainderMap.put(remainder, remainderMap.getOrDefault(remainder, 0L) + 1);
+        for(int i=1; i<=N; i++){
+            mod = (mod+Integer.parseInt(st.nextToken()))%M;
+            modulo[mod]++;
         }
 
+        count += modulo[0];
+        for(int i=0; i<M; i++){
+            count += modulo[i]*(modulo[i]-1)/2;
+        }
         System.out.println(count);
     }
 }
