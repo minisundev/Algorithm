ea7ff2e3cceeabfd82b1e73242cf3ca368ae25de
Minsun <58019747+minisundev@users.noreply.github.com>
2024-07-12 22:25:34 +0900

    [Gold V] Title: 평범한 배낭, Time: 128 ms, Memory: 14520 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] weights = new int[N];
+        int[] values = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            weights[i] = Integer.parseInt(st.nextToken());
+            values[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // DP 배열
+        int[] dp = new int[K + 1];
+
+        // 최적해 구하기
+        for(int i = 0; i < N; i++) {
+            for(int w = K; w >= weights[i]; w--) {
+                dp[w] = Math.max(dp[w], dp[w - weights[i]] + values[i]);
+            }
+        }
+
+        System.out.println(dp[K]);
+    }
+}
