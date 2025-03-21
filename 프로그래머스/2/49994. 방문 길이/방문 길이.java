ed514e609a1dad7f02a83295f515410311577419
Minsun <58019747+minisundev@users.noreply.github.com>
2025-03-21 18:43:22 +0900

    [level 2] Title: 방문 길이, Time: 2.75 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,57 +1,77 @@
-import java.lang.Math;
+import java.util.*;
 
 class Solution {
+    boolean [][] v;
+    boolean [][] h;
+    int max = 5;
+    
     public int solution(String dirs) {
+        v = new boolean [11][10];
+        h = new boolean [10][11];
+        Point p = new Point(0,0);
         int answer = 0;
-        boolean [][] visitVertical = new boolean [11][10];
-        boolean [][] visitHorizontal = new boolean [10][11];
         
-        int x = 5;
-        int y = 5;
-        
-        for(int i=0;i<dirs.length();i++){
+        for(int i=0; i<dirs.length(); i++){
             char c = dirs.charAt(i);
-            int X=x;
-            int Y=y;
-            
-            boolean horizontal = false;
-            boolean vertical = false;
-            
-            if(c=='U'){
-                Y++;
-                vertical = true;
-            }else if(c=='D'){
-                Y--;
-                vertical = true;
-            }else if(c=='L'){
-                X--;
-                horizontal = true;
-            }else if(c=='R'){
-                X++;
-                horizontal = true;
-            }
-            
-            if(X>=0&&X<11&&Y>=0&&Y<11){
-                
-                if(horizontal){
-                    if(!visitHorizontal[Math.min(X,x)][Y]){
-                        visitHorizontal[Math.min(X,x)][Y] = true;
-                        answer++;
-                    }
-                    
-                }else if(vertical){
-                    if(!visitVertical[X][Math.min(Y,y)]){
-                        visitVertical[X][Math.min(Y,y)] = true;
-                        answer++;
-                    }
-                    
-                }
-                
-                                
-                x = X;
-                y = Y;
+            if(move(p,c)){
+                answer++;
             }
         }
+        
         return answer;
+    }
+    
+    boolean move(Point p, char c){
+        boolean result = false;
+        int vX = p.x;
+        int vY = p.y;
+        int hX = p.x;
+        int hY = p.y;
+        boolean vF = false;
+        boolean hF = false;
+        
+        if(c=='U' && p.y+1<=max){
+            p.y = p.y+1;
+            vX += 5;
+            vY += 5;
+            vF = true;
+        }else if(c=='D'&& p.y-1>=-max){
+            p.y = p.y-1;
+            vX += 5;
+            vY += 4;
+            vF = true;
+        }else if(c=='L'&& p.x-1>=-max){
+            p.x = p.x-1;
+            hY += 5;
+            hX += 4;
+            hF=true;
+        }else if(c=='R'&& p.x+1<=max){
+            p.x = p.x+1;
+            hY += 5;
+            hX += 5;
+            hF=true;
+        }
+        if(vF){
+            if(!v[vX][vY]){
+                v[vX][vY] = true;
+                result = true;
+            }
+        }
+        if(hF){
+            if(!h[hX][hY]){
+                h[hX][hY] = true;
+                result = true;
+            }
+        }
+        return result;
+    }
+}
+
+class Point{
+    public int x;
+    public int y;
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
     }
 }
