3310a3e29310290746f2e8c392ade6372ef45253
Minsun <58019747+minisundev@users.noreply.github.com>
2025-11-18 23:57:42 +0900

    [level 1] Title: 실패율, Time: 15.36 ms, Memory: 96.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int [] hit = new int [N+1];
+        //그 스테이지에 있는애들 다 전번 넘어왔을테니까 뒤부터 누적합으로 처리해~
+        for(int i : stages){
+            hit[i-1] = hit[i-1]+1;
+        }
+        
+        int [][] result = new int[N][3];
+        
+        PriorityQueue<Rate> pq = new PriorityQueue<>(
+        (o1,o2)->{
+            if(o2.rate!=o1.rate){
+                if(o2.rate-o1.rate>0)return 1;
+                if(o2.rate-o1.rate<0)return -1;
+            }
+            return o1.index-o2.index;
+        });
+        
+        double total = hit[hit.length-1];
+        
+        for(int i=hit.length-2; i>=0; i--){
+            total += hit[i];
+            Rate r = new Rate();
+            r.index = i;
+            if(hit[i]==0){
+                r.rate = 0;
+            }else{
+                r.rate = hit[i]/total;
+            }
+            pq.offer(r);
+        }
+        
+        int [] answer = new int [N];
+        for(int i=0;i<answer.length; i++){
+            answer[i] = pq.poll().index+1;
+        }
+        return answer;
+    }
+}
+class Rate{
+    public int index;
+    public double rate;
+    public Rate(){};
+}
